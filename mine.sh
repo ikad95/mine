dc7f3c2d8e2c3e88ee2569842a98b4dc6ac262a9
Moulik Adak <moulik.adak@gmail.com>
2017-08-09 17:25:51 +0530

    Ok

---
+++ mine.sh
@@ -0,0 +1,1 @@
+ethminer -t 8 -C -F http://eth.pool.minergate.com:55751/moulik.adak@gmail.com --disable-submit-hashrate
---
+++ miner_install.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo apt-get -f install software-properties-common
+sudo apt-get -f install
+sudo add-apt-repository ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install ethminer
+
+
+
+
